e38295c98aeadd109c7470fc35ee9c0f55f988c4
mahdikheirkhah <mohammad.kheirkhah@gritlab.ax>
2025-06-12 06:19:47 +0300

    functions declartion for dom.js

---
+++ framework/dom.js
@@ -0,0 +1,15 @@
+function createElement(tag, attrs = {}, children = []){
+
+}
+
+function applyAttributes(element, attrs) {
+
+}
+
+function createDomElement(vnode){}
+
+function mount(vnode, parentDomElement){
+
+}
+
+function patch(oldVnode, newVnode) {}
